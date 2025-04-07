f04adb165ecdc6422f73052aba19bb51fbde53b5
gracepogii <marianngrace.dizon01@student.csulb.edu>
2025-04-06 21:44:06 -0700

    indents

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 import socket
+
 ip_address = "0.0.0.0"
 port_number = 8080
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-server_socket.bind((ip_address, port_number))
-server_socket.listen()
-print(f"Server is listening on {ip_address}:{port_number}")
-conn, addr = server_socket.accept()
-with conn:
-print(f"Connected to {addr}")
-while True:
-data = conn.recv(1024)
-if not data:
-break
-response = data.decode().upper()
-conn.sendall(response.encode())
+    server_socket.bind((ip_address, port_number))
+    server_socket.listen()
+    print(f"Server is listening on {ip_address}:{port_number}")
+    conn, addr = server_socket.accept()
+    with conn:
+        print(f"Connected to {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            response = data.decode().upper()
+            conn.sendall(response.encode())
